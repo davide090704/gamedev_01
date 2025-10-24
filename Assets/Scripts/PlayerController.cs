537ce5d3f553dbc77cc32fe85d826fa84aea974f
Davide Caporale <davide_caporale@studenti.naba.it>
2025-10-24 09:15:37 +0200

    CameraSwitch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
     public float turnSpeed = 45.0f;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,14 +19,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Axis setup
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         
         // Move the vehicle forward
         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+       if(Input.GetKeyDown(switchKey))
+       {
+        mainCamera.enabled = !mainCamera.enabled;
+        hoodCamera.enabled = !hoodCamera.enabled;
+       }
+
+
       
 
     }
 }
-;
